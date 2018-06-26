d273d67ea186aa3607bc2ce627f8deabe8112dfa
Jordan S Read <jread@usgs.gov>
2018-06-26 08:19:20 -0500

    updates for model runs

--- R/model_file_prep.R
+++ R/model_file_prep.R
@@ -5,8 +5,23 @@ get_driver_file <- function(filepath, nhd_id){
   file.copy(from = temp_path, to = filepath)
 }
 
-get_nml_file <- function(filepath, nhd_id, meteopath){
-  nml <- populate_base_lake_nml(site_id = nhd_id, 
+get_nml_file <- function(filepath, nhd_id, meteopath, ...){
+  
+  nml <- populate_base_lake_nml(site_id = nhd_id, ...,
                                 driver = meteopath)
   write_nml(glm_nml = nml, file = filepath)
+}
+
+run_export_temp <- function(outfile, nmlpath, simnml, ...){
+  
+  nml <- read_nml(nmlpath)
+  nml <- set_nml(nml, arg_list = list(...))
+  write_nml(glm_nml = nml, file = simnml)
+  run_glm('data')
+  unlink(simnml)
+  temp_data <- get_temp(reference = 'surface')
+  plot_temp()
+  
+  readr::write_tsv(x = temp_data, path = outfile)
+  
 }
